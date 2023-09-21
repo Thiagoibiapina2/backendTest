51b63572583b9b6582c40c2048951239b65d0c24
thiago aguiar <thiago.aguiar879@gmail.com>
2023-09-21 18:30:49 -0400

    correction

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 
-import router from './routes'
+import router from './src/routes'
 import 'dotenv/config'
 
 const app = express().use(cors())
