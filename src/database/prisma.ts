04a2547f823ff6d6cf704ea2b93f4810ef5d32ea
thiago aguiar <thiago.aguiar879@gmail.com>
2023-09-25 16:03:02 -0400

    prisma.ts

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -2,6 +2,11 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient({
   log: ['error', 'info', 'query', 'warn'],
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
 })
 
 async function testDatabaseConnection() {
