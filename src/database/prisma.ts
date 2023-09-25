ffc4460cab3b46225305eb7b6da81197977be105
thiago aguiar <thiago.aguiar879@gmail.com>
2023-09-25 11:36:05 -0400

    prisma.ts

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -4,4 +4,17 @@ const prisma = new PrismaClient({
   log: ['error', 'info', 'query', 'warn'],
 })
 
+async function testDatabaseConnection() {
+  try {
+    await prisma.$connect()
+    console.log('Conexão com o banco de dados estabelecida com sucesso')
+  } catch (error) {
+    console.error('Erro ao conectar ao banco de dados:', error)
+  } finally {
+    await prisma.$disconnect()
+  }
+}
+
+testDatabaseConnection()
+
 export { prisma }
