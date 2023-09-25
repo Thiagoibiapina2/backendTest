2bfb20c083d61809f25838c39c3cc2be9595d543
thiago aguiar <thiago.aguiar879@gmail.com>
2023-09-25 11:02:11 -0400

    server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,15 @@ import cors from 'cors'
 import router from './routes'
 import 'dotenv/config'
 
-const app = express().use(cors())
+const app = express()
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000', // Substitua isso pela origem da sua aplicação web
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Se você estiver usando cookies ou autenticação com credenciais
+  }),
+)
 
 app.use(express.json())
 
