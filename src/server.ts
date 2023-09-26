cbd1b6a4fdb1718c64d5b34bd40ec50d70b5d78b
thiago aguiar <thiago.aguiar879@gmail.com>
2023-09-26 11:01:32 -0400

    server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,8 @@ import 'dotenv/config'
 
 const app = express()
 
+const port = process.env.PORT || 3002
+
 app.use(
   cors({
     origin: 'http://localhost:3000', // Substitua isso pela origem da sua aplicação web
@@ -18,6 +20,6 @@ app.use(express.json())
 
 app.use(router)
 
-app.listen(3002, function () {
+app.listen(port, function () {
   console.log('Servidor Online!')
 })
